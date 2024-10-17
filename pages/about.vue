73abaef0f9307d5fc4d593a001b79f4df94d5376
Sky Blue <dawadiaakash@gmail.com>
2024-10-17 23:52:19 +0545

    feat: add svg icons to navbar li

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,27 @@
 <template>
-    <h1 class="m-20">This is About page.</h1>
+    <div>
+        <main class="pt-8 pb-16 lg:pt-16 mt-5 lg:pb-24 bg-blue-200 dark:bg-green-100 min-h-screen antialiased"> 
+        <p class="text-blue-300-5xl"> <strong class="text-xl font-bold"> Once upon a time,</strong> inside a country called Nepal, in the shadow of the Himalayas, there was a boy who asked
+            way too many questions, making his parents and teachers mad. Then, as the years went by he stopped asking
+            silly questions. And started asking sillier ones - questions to do with life, death, coffee, semicolons and
+            such.
+        </p>
+        <br />
+        <strong class="text-color-blue-700-5xl">Hey,</strong>  I am <span className="purple">Aakash Dawadi </span>
+        from <span className="purple"> Chitwan,Nepal.</span>
+        <br />
+        I am currently a student at TU-IOST.
+        <br />
+        I enjoy solving bugs and logical challanges.
+        <br />
+        <br /> Within the intricate dance of algorithms and codes, I find my passion ignited, constantly seeking to
+        unravel the complexities that define the backbone of digital systems. Whether it's developing sleek and
+        efficient software solutions or architecting robust network infrastructures, I am driven by the pursuit of
+        excellence and the desire to contribute meaningfully to the technological landscape.
+        <br />
+    </main>
+
+    </div>
+
+
 </template>
