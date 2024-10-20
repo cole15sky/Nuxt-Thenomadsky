b23191f0f3f582de127c6085390eac646e3c159d
Sky Blue <dawadiaakash@gmail.com>
2024-10-20 12:16:15 +0545

    feat: add components to articles page

---
+++ components/ArticlesHome.vue
@@ -0,0 +1,55 @@
+<template>
+    <main
+        class="pt-8 pb-16 lg:pt-16 mt-14 lg:pb-24 bg-gradient-to-r from-blue-200 to-blue-400 dark:bg-gradient-to-r dark:from-green-200 dark:to-green-400 min-h-screen antialiased">
+        <div class=" grid md:grid-cols-2">
+            <!-- Content Section -->
+            <div class="p-4">
+                <h1 class=" flex justify-center text-3xl pl-4 font-bold ">Sunset from the valley.</h1>
+                <p class="text-xl p-4 ">
+                    There’s a quiet magic in nature that has always fascinated me. From the gentle sway of trees to the
+                    vastness of open skies, I’ve found peace in the simplicity of the natural world. Roaming through
+                    valleys, listening to the rhythm of flowing rivers, or gazing at the distant mountains, I feel
+                    deeply connected to the earth. It’s in these moments, away from the rush of life, that I discover
+                    clarity and a sense of belonging that no other place offers. Nature reminds me to slow down and
+                    appreciate the beauty of the present moment.
+
+                    <br>
+                    Nature is my refuge, where I find balance and inspiration. Whether it’s walking under a canopy of
+                    trees or sitting quietly by a lake, it’s in these spaces that my thoughts flow freely. The world
+                    feels bigger, yet somehow more personal, as if each part of nature has its own story to tell. I take
+                    great joy in being a part of this narrative, soaking in the lessons it quietly imparts. Nature fuels
+                    my creativity, restores my energy, and constantly reminds me that life’s simplest pleasures are
+                    often the most profound.
+                </p>
+            </div>
+
+
+            <div>
+                <!-- Images Section -->
+                <div class="pr-5 pl-5 mt-20 grid grid-cols-1 gap-6">
+                    <img class="w-full object-cover md:grid-cols-1" src="/Assets/Images/sunset-valley.jpg"
+                        alt="valley pic">
+                </div>
+
+            </div>
+        </div>
+        <p class="text-xl p-4 ">
+            Watching a sunset, especially from the heart of a valley, brings me a deep sense of satisfaction.
+            The sky’s vibrant transformation as the sun dips below the horizon feels like a daily masterpiece
+            painted just for us. Each sunset is unique, a fleeting moment that reminds me of the transient yet
+            precious nature of time. The stillness that surrounds me as the colors shift and fade gives me a
+            sense of tranquility, as if the world is at peace, even if just for a while.
+
+        </p>
+    </main>
+</template>
+
+<style scoped>
+h1 {
+    font-family: EB Garaond;
+}
+
+p {
+    font-family: Poppins;
+}
+</style>
--- pages/articles/articles.vue
+++ pages/articles/articles.vue
@@ -1,5 +1,8 @@
 <template>
-  <main class="pt-8 pb-16 lg:pt-16 mt-14 lg:pb-24 bg-blue-100 dark:bg-blue-100 antialiased">
+  <div>
+    <ArticlesHome/>
+  </div>
+  <main class=" pb-16 lg:pt-16  lg:pb-24 bg-blue-100 dark:bg-blue-100 antialiased">
     <div class="flex justify-between px-4 mx-auto max-w-screen-xl">
       <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue yellow:format-invert">
         <header class="mb-4 lg:mb-6 not-format">
