0dc898bcdbccbe24463f56186e7d492d51e22e6b
Sky Blue <dawadiaakash@gmail.com>
2024-10-20 22:43:59 +0545

    chore: gallery updates

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,27 +1,58 @@
+<script setup>
+import { ref } from 'vue';
+
+const images = ref([
+  
+  { path: '/Images/sunset-valley.jpg', orientation: 'landscape' },
+  { path: '/Images/sunsethome.jpeg', orientation: 'landscape' },
+  { path: '/Images/ktm.jpg', orientation: 'landscape' },
+
+  { path: '/Images/IMG_2903_Original.jpg', orientation: 'portrait' },
+  { path: '/Images/20240421_164800_Original.jpg', orientation: 'portrait' },
+  { path: '/Images/IMG_6362_Original.jpg', orientation: 'portrait' },
+
+  { path: '/Images/sauraha.jpg', orientation: 'landscape' },
+  { path: '/Images/12.jpg', orientation: 'landscape' },
+  { path: '/Images/yak-kharka.jpg', orientation: 'landscape' },
+
+  { path: '/Images/IMG_9398_Original.jpg', orientation: 'portrait' },
+  { path: '/Images/IMG_4823.JPG', orientation: 'portrait' },
+  { path: '/Images/IMG_023_Original.jpg', orientation: 'portrait' },
+
+  { path: '/Images/IMG_04_Original.jpg', orientation: 'landscape' },
+  { path: '/Images/IMG_092_Original.jpg', orientation: 'landscape' },
+  { path: '/Images/IMG_9236_Original.jpg', orientation: 'landscape' }
+  ,
+  { path: '/Images/IMG_4340_Original.jpg', orientation: 'landscape' },
+  { path: '/Images/IMG_3110_Original.jpg', orientation: 'landscape' },
+  { path: '/Images/pul1.JPG', orientation: 'landscape' },
+
+  { path: '/Images/IMG_0599_Original.jpg', orientation: 'landscape' },
+  { path: '/Images/IMG_0457_Original.jpg', orientation: 'landscape' },
+  { path: '/Images/IMG_5508_Original.jpg', orientation: 'landscape' },
+  
+//   { path: '/Images/20240423_230912.jpg', orientation: 'landscape' },
+  
+
+
+]);
+
+// Function to return grid class based on image orientation
+const getGridClass = (image) => {
+  return image.orientation === 'portrait' ? 'col-span-1 row-span-2' : 'col-span-1 row-span-1';
+};
+</script>
+
 <template>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-4">
       <div
         v-for="(image, index) in images"
         :key="index"
         class="overflow-hidden rounded-lg transform transition-transform duration-300 hover:scale-105"
+        :class="getGridClass(image)"
       >
-        <img :src="image" alt="Gallery Image" class="w-full h-auto" />
+        <img :src="image.path" alt="Gallery Image" class="w-full h-full object-cover" />
       </div>
-      <!-- <img :src="images[0]" alt=""> -->
     </div>
   </template>
   
-  <script setup>
-  
-  // Create an array of image paths
-  const images = ref([
-  ('/Images/sunset-valley.jpg'),
-  ('/Images/sunsethome.jpeg'),
-  ('/Images/ktm.jpg'),
-  ('/Images/sauraha.jpg'),
-  ('/Images/12.jpg'),
-  ('/Images/yak-kharka.jpg'),
-  ('/Images/pul1.JPG'),
-]);
-  </script>
- 
