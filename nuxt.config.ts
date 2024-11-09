eaece1d97a4faab444d176daefd8f6399ec3c601
Sky Blue <dawadiaakash@gmail.com>
2024-11-09 14:16:11 +0545

    chore: update bg-light to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: [
-    'swiper/swiper-bundle.css'
+    'swiper/swiper-bundle.css',
+     'assets/global.css'
+
   ],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: [ '@nuxt/fonts', '@nuxt/ui']
+  modules: [ '@nuxt/fonts', '@nuxt/ui'],
+  
 })
